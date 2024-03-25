edaaaddf5f22fb610bc7b1511e50f2486940eafb
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2024-03-25 16:13:37 +0200

    Fetching product's price

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -5,6 +5,7 @@ import { getSession } from "~/services/session.server";
 import { getUserById } from "~/models/user.server";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { scrapeAmazonProductPrice } from "~/services/scrapeAmazonProduct"
 
 export default function () {
   const user = useLoaderData<typeof loader>();
@@ -45,5 +46,8 @@ export async function loader({ request }: LoaderArgs) {
   const userId = session.get("user").id;
   const user = await getUserById(userId);
 
+  const amazonProductUrl = "https://www.amazon.com/BENGOO-G9000-Controller-Cancelling-Headphones/dp/B01H6GUCCQ/ref=sr_1_1_sspa?_encoding=UTF8&content-id=amzn1.sym.12129333-2117-4490-9c17-6d31baf0582a&dib=eyJ2IjoiMSJ9.LzA2u5MEtzOJYnnhfYifOZF03L02bnVOQQ7KexwWTOTO8pEy-MsB9UBcp4zBiOuPrVM64KhjXtaMqXW8Sv_uDglhXacF_yWckAJtJyVCaoHSIpYtd9yGN4bVy_IiuFHUO_dowrNRBCXItB1iZh8B0Xk0us67ap4I8D1cDsiF0UYVxY5VVIEh_JzfxVeUNh7E6dLtGdqDzyzPk_r18Irhq9CtgdIWQahDIIxMKicYdjQ.QMWMlQHixcVHmdEo7Pr1okyY6BXhYIJU-ni21Z3PO2k&dib_tag=se&keywords=gaming%2Bheadsets&pd_rd_r=45915236-de2f-4fbf-a451-260f114f79db&pd_rd_w=Ab88c&pd_rd_wg=FAV4a&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=QP7C3PYEB8BARBDGHY67&qid=1711375535&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1";
+  const amazonProductPrice = await scrapeAmazonProductPrice(amazonProductUrl);
+  
   return json(user);
 }
---
+++ app/services/scrapeAmazonProduct.ts
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+export async function scrapeAmazonProductPrice(url: string) {
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+
+    const $ = cheerio.load(html);
+
+    const priceElement = $('span.a-price span.a-offscreen');
+    const priceText = priceElement.text().trim();
+
+    console.log("PRICE TEXT:", priceText);
+    
+    return priceText;
+  } catch (error) {
+    console.error("Error fetching Amazon product page:", error);
+    return null;
+  }
+}
